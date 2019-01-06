6f9feb3e6d9650a92c5b43b63a354d9d46b79bae
Oleksandr Shaposhnikov <o.shaposhnikov-2020@kpi.ua>
2019-01-06 16:29:56 +0200

    Initial renderer implementation

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-    std::shared_ptr<sf::Image> image = std::make_shared<sf::Image>();
-    image->create(window.getSize().x, window.getSize().y, sf::Color::Black);
 
-    AsyncMandelbrotRenderer renderer(image);
+    using Renderer = AsyncMandelbrotRenderer<double>;
+    using PaneCoords =  Renderer::PaneCoords;
+    using ImageCoords = Renderer::ImageCoords;
+
+    Renderer renderer;
     ImageCoords img { {0, 0}, window.getSize() };
-    PaneCoords pane { {-2.0, 1.0}, {-1.5, 1.5} };
+    PaneCoords pane { {-1.5, 1.5}, {-2.0, 1.0} };
+
+    renderer.set_palette(palette::grayscale);
 
     sf::Texture texture;
-    sf::Sprite sprite;
+    texture.create(window.getSize().x, window.getSize().y);
+
+    sf::Sprite sprite(texture);
 
     while (window.isOpen())
     {
@@ -27,15 +33,11 @@ int main()
         }
 
         // render to image
-        auto future = renderer.render_async(img, pane);
-
-        // ...
+        auto future = renderer.render_async(img, pane, 9000);
 
         window.clear();
-        future.wait();
 
-        texture.update(*image);
-        sprite.setTexture(texture);
+        texture.update(future.get());
 
         window.draw(sprite);
         window.display();
