9827a8659c1056294d63d8adfdc5a36b0b300100
Tsiura Vasyl <morfey.rulit@gmail.com>
2019-01-08 15:42:52 +0200

    Revert "Removed unused files."
    This reverts commit 67e8809d818728c13ad16db0c2d260ea1a26ae8a.

---
+++ event_helper.h
@@ -0,0 +1,18 @@
+#ifndef EVENT_CPP
+#define EVENT_CPP
+
+#include "main_loop_helper.h"
+
+class EventHelper
+{
+    sf::RenderWindow *window;
+
+  public:
+    EventHelper(sf::RenderWindow *window);
+    void processEvents(MainLoopHelper *helper);
+};
+
+Rectangle<double> scaleCoordinates(Rectangle<double> initialCoords,
+                                   Rectangle<unsigned int> imgCoorgs,
+                                   Rectangle<unsigned int> newRegion);
+#endif
